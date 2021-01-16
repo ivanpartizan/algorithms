4b82cb2500fc1229ade97f3b65809989e4fe9ce3
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-01-16 04:45:16 +0100

    update

--- functions.js
+++ functions.js
@@ -17,3 +17,34 @@ const sumDigPow = (a, b) => {
 };
 
 sumDigPow(1, 150);
+
+// Roman Numerals Encoder
+
+const romanEncoder = (number) => {
+  let romanNumbers = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = "";
+  for (let key in romanNumbers) {
+    while (number >= romanNumbers[key]) {
+      roman += key;
+      number -= romanNumbers[key];
+    }
+  }
+  return roman;
+};
+
+romanEncoder(2020);
