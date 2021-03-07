1aabe3ea2a1225e31c9ee181b46467a397a3e11c
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-03-07 23:17:27 +0100

    practice

--- practice.js
+++ practice.js
@@ -304,4 +304,17 @@ let strings = [
 unique(strings);
 
 // Create keyed object from array
-function groupById(arr) {}
+function groupById(arr) {
+  return arr.reduce((object, current) => {
+    object[current.id] = current;
+    return object;
+  }, {});
+}
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
