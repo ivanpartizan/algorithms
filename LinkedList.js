72e130d40f7bf00e782e35534b280874bee7ea38
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-03-08 04:35:46 +0100

    practice

---
+++ LinkedList.js
@@ -0,0 +1,30 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function (value) {
+  let newNode = new Node(value, this.head, null);
+
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+
+  this.head = newNode;
+};
+
+let ll = new LinkedList();
+
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+
+console.log(ll);
