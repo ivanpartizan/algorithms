9d2e49da7a852ad5dd2d5c86a59762a6a4fa1097
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-03-08 23:31:43 +0100

    practice

--- LinkedList.js
+++ LinkedList.js
@@ -21,10 +21,89 @@ LinkedList.prototype.addToHead = function (value) {
   this.head = newNode;
 };
 
+LinkedList.prototype.addToTail = function (value) {
+  let newNode = new Node(value, null, this.tail);
+
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+
+  this.tail = newNode;
+};
+
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) {
+    return null;
+  }
+
+  let val = this.head.value;
+  this.head = this.head.next;
+
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+
+  return val;
+};
+
+LinkedList.prototype.removeTail = function () {
+  if (!this.tail) {
+    return null;
+  }
+
+  let val = this.tail.value;
+  this.tail = this.tail.prev;
+
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+
+  return val;
+};
+
+LinkedList.prototype.search = function (searchValue) {
+  let currentNode = this.head;
+
+  while (currentNode) {
+    if (currentNode.value === searchValue) {
+      return currentNode.value;
+    }
+    currentNode = currentNode.next;
+  }
+
+  return null;
+};
+
+LinkedList.prototype.indexOf = function (value) {
+  let indexes = [];
+  let currentIndex = 0;
+  let currentNode = this.head;
+
+  while (currentNode) {
+    if (currentNode.value === value) {
+      indexes.push(currentIndex);
+    }
+    currentNode = currentNode.next;
+    currentIndex++;
+  }
+
+  return indexes;
+};
+
 let ll = new LinkedList();
 
 ll.addToHead(100);
 ll.addToHead(200);
 ll.addToHead(300);
-
+ll.addToTail(10);
+ll.addToTail(20);
+ll.addToTail(30);
+console.log(ll.search(100));
+console.log(ll.search(1000));
 console.log(ll);
