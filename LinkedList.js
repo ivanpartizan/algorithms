d0df3732a6cf641b0424ab9e03e6ea0593200369
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-03-25 21:47:40 +0100

    update

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,10 @@
-class Node {}
+class Node {
+  constructor(value, prev, next) {
+    this.value = value;
+    this.prev = prev || null;
+    this.next = next || null;
+  }
+}
 
 class LinkedList {}
 
--- recursion.js
+++ recursion.js
@@ -1,3 +1,8 @@
+// 1. Identify the base case
+// 2. Identify the recursive case
+// 3. Get closer and closer to the base case and return when needed
+// Usually 2 returns, one for base case and one for recursive case
+
 // Sum all numbers till the given one
 // Using a for loop
 function sumTo(n) {
@@ -11,3 +16,24 @@ function sumTo(n) {
 sumTo(100);
 
 // Using a recursion
+
+// Factorial
+function findFactorialIterative(number) {
+  let factorial = 1;
+  for (let i = 2; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+findFactorialIterative(5);
+
+function findFactorialRecursive(number) {
+  if (number == 1 || number == 0) {
+    return 1;
+  } else {
+    return number * findFactorialRecursive(number - 1);
+  }
+}
+
+findFactorialRecursive(5);
