589a9fff106d91a440ba64a0b4a06f59ebd4e8a4
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-04-03 13:38:03 +0200

    update

--- functions.js
+++ functions.js
@@ -346,3 +346,23 @@ function calc(x) {
 
   return total1Sum - total2Sum;
 }
+
+// Are they the "same"?
+function comp(array1, array2) {
+  if (
+    array1 === null ||
+    array2 === null ||
+    array1 === [] ||
+    array2 === [] ||
+    array1.length !== array2.length
+  ) {
+    return false;
+  }
+
+  return array1
+    .map((value) => value * value)
+    .sort()
+    .toString() === array2.sort().toString()
+    ? true
+    : false;
+}
