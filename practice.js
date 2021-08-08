818e3029211184aafd78a3a21fbe54b0e6a2094c
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-08-08 20:48:22 +0200

    update

--- practice.js
+++ practice.js
@@ -391,3 +391,31 @@ function noOdds(values) {
   }
   return array;
 }
+
+// JSON
+let user = {
+  name: "John Smith",
+  age: 35,
+};
+
+let obj = JSON.parse(JSON.stringify(user));
+
+let room = {
+  number: 23,
+};
+
+let meetup = {
+  title: "Conference",
+  occupiedBy: [{ name: "John" }, { name: "Alice" }],
+  place: room,
+};
+
+// circular references
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(
+  JSON.stringify(meetup, function replacer(key, value) {
+    /* your code */
+  })
+);
