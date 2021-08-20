794f3421c5b48a86e58d30ad07fd9ec38d7414f5
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-08-20 03:48:52 +0200

    update

--- delays.js
+++ delays.js
@@ -1,3 +1,4 @@
+// Output every second
 function printNumbers(from, to) {
   let start = from;
 
@@ -11,3 +12,17 @@ function printNumbers(from, to) {
 }
 
 printNumbers(1, 10);
+
+function printNumbers(from, to) {
+  let start = from;
+
+  setTimeout(function timer() {
+    alert(start);
+    if (start < to) {
+      setTimeout(timer, 1000);
+    }
+    start++;
+  }, 1000);
+}
+
+printNumbers(1, 5);
