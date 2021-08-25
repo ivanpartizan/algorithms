303592ba18ed8de46e5f0a14ac18755cee5c3f18
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-08-25 23:34:22 +0200

    update

--- delays.js
+++ delays.js
@@ -3,8 +3,7 @@ function printNumbers(from, to) {
   let start = from;
 
   let timer = setInterval(function () {
-    // alert(start);
-    console.log(start);
+    alert(start);
     if (start == to) {
       clearInterval(timer);
     }
@@ -18,7 +17,7 @@ function printNumbers(from, to) {
   let start = from;
 
   setTimeout(function timer() {
-    // alert(start);
+    alert(start);
     if (start < to) {
       setTimeout(timer, 1000);
     }
@@ -27,3 +26,12 @@ function printNumbers(from, to) {
 }
 
 printNumbers(1, 5);
+
+let i = 0;
+
+setTimeout(() => alert(i), 100); // ?
+
+// assume that the time to execute this function is >100ms
+for (let j = 0; j < 100000000; j++) {
+  i++;
+}
