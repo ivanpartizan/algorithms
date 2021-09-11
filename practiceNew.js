d248090183bfbe2513758c541c9f3148784984b8
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-09-11 23:01:55 +0200

    update

--- practiceNew.js
+++ practiceNew.js
@@ -84,6 +84,23 @@ function findOdd(A) {
 
 findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]);
 
+// Integer Difference
+const intDiff = (arr, n) => {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[i] - arr[j]) == n) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+intDiff([1, 1, 5, 6, 9, 16, 27], 4);
+
 // Vasya - Clerk
 function tickets(peopleInLine) {
   let bill25 = 0;
