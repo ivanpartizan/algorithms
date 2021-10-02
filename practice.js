17cbbce865d36de01e5102b382598f4d79aea92f
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-10-02 22:51:48 +0200

    update

--- practice.js
+++ practice.js
@@ -420,3 +420,25 @@ function swapVowelCase(str) {
 }
 
 swapVowelCase("C Is AlIvE!");
+
+// Output prime numbers
+function showPrimes(n) {
+  let primes = [];
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+showPrimes(100);
