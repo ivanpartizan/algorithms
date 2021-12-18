8759e0d507e693cb84e69d7be84dda21fd17ab37
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-12-18 22:15:10 +0100

    update

--- practice2.js
+++ practice2.js
@@ -145,3 +145,16 @@ function present(x, y) {
 }
 
 present("goodpresent", 9);
+
+// Double Trouble
+function trouble(x, t) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] + x[i + 1] == t) {
+      x.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return x;
+}
+
+trouble([4, 1, 1, 1, 4], 2);
