1b036420481ba6ccc3134be1794c416637ab4559
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-04-27 21:07:36 +0200

    update

--- practice5.js
+++ practice5.js
@@ -1,3 +1,37 @@
+// IP Validation
+function isValidIP(str) {
+  console.log(str);
+  if (
+    str == "" ||
+    str.includes(" ") ||
+    str.includes(",") ||
+    /\r|\n/.test(str) ||
+    /[a-zA-Z]/.test(str)
+  ) {
+    return false;
+  } else {
+    let octets = str.split(".");
+
+    if (
+      octets.length != 4 ||
+      octets.some(
+        (value) =>
+          (value.length > 1 && value[0] == 0) || value == "" || value == "-0"
+      )
+    ) {
+      return false;
+    } else {
+      if (octets.every((value) => value >= 0 && value < 256)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+isValidIP("12.255.56.1");
+
 // Narcissistic Numbers
 function isNarcissistic(n) {
   let numberLength = String(n).length;
