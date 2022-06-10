50dfbf203d02da02d870db4b76068ff5d29c9107
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-06-10 05:05:37 +0200

    update

--- recursion1.js
+++ recursion1.js
@@ -448,3 +448,101 @@ function camelize(str) {
 camelize("background-color") == "backgroundColor";
 camelize("list-style-image") == "listStyleImage";
 camelize("-webkit-transition");
+
+function filterRange(arr, a, b) {
+  return arr.slice().filter((num) => num >= a && num <= b);
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRange(arr, 1, 4);
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+alert(arr);
+
+let arr = [5, 2, 1, -10, 8];
+arr.sort((a, b) => b - a);
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted); // CSS, HTML, JavaScript
+alert(arr); // HTML, JavaScript, CSS (no changes)
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+
+let names = users.map((user) => user.name);
+
+console.log(names);
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+let usersMapped = users.map((user) => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id,
+}));
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+console.log(usersMapped);
+alert(usersMapped[0].id); // 1
+alert(usersMapped[0].fullName); //
+
+function sortByAge(users) {
+  return users.sort((a, b) => a.age - b.age);
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [pete, john, mary];
+
+let sorted = sortByAge(arr);
+console.log(sorted);
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
+
+function shuffle(array) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    console.log(array[i]);
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+let arr = [1, 2, 3];
+
+shuffle(arr);
