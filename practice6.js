2bbfe3ff7a2f440351130b7296d68af0e6ee2cb8
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-06-21 23:17:42 +0200

    update

--- practice6.js
+++ practice6.js
@@ -11,3 +11,16 @@ function leastLarger(a, i) {
 }
 
 leastLarger([4, 1, 3, 5, 6], 0);
+
+// SevenAte9
+function sevenAte9(str) {
+  str = Array.from(str);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == 7 && str[i + 1] == 9 && str[i + 2] == 7) {
+      str.splice(i + 1, 1);
+    }
+  }
+  return str.join("");
+}
+
+sevenAte9("79712312");
