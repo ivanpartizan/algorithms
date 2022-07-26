d584159eebc19b3081bdf38a97b9884a7f7d6eee
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-07-26 23:03:45 +0200

    update

--- practice6.js
+++ practice6.js
@@ -67,3 +67,53 @@ function heron(a, b, c) {
 }
 
 heron(3, 4, 5);
+
+// “All vanity plates must start with at least two letters.”
+// “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+// “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+// “No periods, spaces, or punctuation marks are allowed.”
+// In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or Invalid if it does not.
+
+function validPlates(plate) {
+  // “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+  if (plate.length < 2 || plate.length > 6) {
+    return "Not Valid";
+  }
+
+  let numbersPart = "";
+
+  for (let i = 0; i < plate.length; i++) {
+    // “No periods, spaces, or punctuation marks are allowed.”
+    if (
+      plate.codePointAt(i) < 48 ||
+      (plate.codePointAt(i) > 57 && plate.codePointAt(i) < 65) ||
+      plate.codePointAt(i) > 90
+    ) {
+      return "Not Valid";
+    }
+    // “All vanity plates must start with at least two letters.”
+    if (isFinite(plate[0]) || isFinite(plate[1])) {
+      console.log(plate[i]);
+      return "Not Valid";
+    }
+    if (isFinite(plate[i])) {
+      numbersPart += plate[i];
+    }
+  }
+
+  console.log(numbersPart);
+  // The first number used cannot be a ‘0’.”
+  if (numbersPart[0] === "0") {
+    return "Not Valid";
+  }
+  // Numbers cannot be used in the middle of a plate; they must come at the end.
+  if (!plate.endsWith(numbersPart[numbersPart.length - 1])) {
+    console.log(numbersPart[numbersPart.length - 1]);
+    return "Valid";
+  }
+
+  if (numbersPart === "") return "Valid";
+  return "Valid";
+}
+
+validPlates("AAA222");
