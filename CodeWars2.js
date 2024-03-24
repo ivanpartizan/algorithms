27e0d4cc61418c8fd99ae13c4fa5f62e5cf3a3fa
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-03-24 04:08:52 +0100

    update

--- CodeWars2.js
+++ CodeWars2.js
@@ -10,3 +10,26 @@ function SeriesSum(n) {
 }
 
 SeriesSum(5);
+
+// Turn any word into a beef taco 7kyu
+function tacofy(word) {
+  let array = word.split("");
+
+  let tacos = array
+    .map((letter) => {
+      if (/[aeiou]/i.test(letter)) return "beef";
+      else if (letter.toLowerCase() == "c") return "cheese";
+      else if (letter.toLowerCase() == "g") return "guacamole";
+      else if (letter.toLowerCase() == "l") return "lettuce";
+      else if (letter.toLowerCase() == "s") return "salsa";
+      else if (letter.toLowerCase() == "t") return "tomato";
+      else return "";
+    })
+    .filter((word) => word.length > 0);
+
+  tacos.push("shell");
+  tacos.unshift("shell");
+  return tacos;
+}
+
+tacofy("ogl");
