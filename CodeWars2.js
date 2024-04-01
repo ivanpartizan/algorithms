4c4c53d3fe82105746626a759db0ff06d5080fd0
Ivan Milenkovic <argentinaivan@hotmail.com>
2024-04-01 20:07:27 +0200

    update

--- CodeWars2.js
+++ CodeWars2.js
@@ -1,3 +1,24 @@
+// Change two-dimensional array 7kyu
+function matrix(array) {
+  array.forEach((row, rowIndex) => {
+    row.forEach((item, itemIndex) => {
+      if (rowIndex == itemIndex) {
+        row[itemIndex] = item < 0 ? 0 : 1;
+      }
+    });
+  });
+
+  return array;
+}
+
+matrix([
+  [-1, 4, -5, -9, 3],
+  [6, -4, -7, 4, -5],
+  [3, 5, 4, -9, -1],
+  [1, 5, -7, -8, -9],
+  [-3, 2, 1, -5, 6],
+]);
+
 // Sum of the first nth term of Series 7kyu
 function SeriesSum(n) {
   let series = [];
