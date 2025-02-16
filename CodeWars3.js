44a57bf1f942438477698f22af3b0b679b0e94c2
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-02-16 23:59:12 +0100

    update

--- CodeWars3.js
+++ CodeWars3.js
@@ -125,3 +125,49 @@ function smallWordHelper(sentence) {
 }
 
 smallWordHelper("The quick brown fox jumps over the lazy dog");
+
+// Turkish Numbers, 0-99 7kyu
+const getTurkishNumber = (num) => {
+  let numbers = {
+    0: "sıfır",
+    1: "bir",
+    2: "iki",
+    3: "üç",
+    4: "dört",
+    5: "beş",
+    6: "altı",
+    7: "yedi",
+    8: "sekiz",
+    9: "dokuz",
+    10: "on",
+    20: "yirmi",
+    30: "otuz",
+    40: "kırk",
+    50: "elli",
+    60: "altmış",
+    70: "yetmiş",
+    80: "seksen",
+    90: "doksan",
+  };
+
+  let turkishNumber = "";
+
+  if (num in numbers) {
+    return numbers[num];
+  } else {
+    let split = String(num)
+      .split("")
+      .map((digit, index) => {
+        if (index == 0) return +(digit + "0");
+        else return +digit;
+      });
+
+    for (let num of split) {
+      turkishNumber += numbers[num] + " ";
+    }
+  }
+
+  return turkishNumber.trimEnd();
+};
+
+getTurkishNumber(12);
