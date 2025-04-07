40e91846adce804e2470c66a741641dcb84687db
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-04-07 23:53:51 +0200

    update

--- CodeWars3.js
+++ CodeWars3.js
@@ -237,6 +237,16 @@ const squareEveryDigit = (x) =>
 
 squareEveryDigit(77455754);
 
+// Find the calculation type 7kyu
+function calcType(a, b, res) {
+  if (a + b === res) return "addition";
+  else if (a - b === res) return "subtraction";
+  else if (a * b === res) return "multiplication";
+  else return "division";
+}
+
+calcType(1, 2, 3);
+
 // Turkish Numbers, 0-99 7kyu
 const getTurkishNumber = (num) => {
   let numbers = {
