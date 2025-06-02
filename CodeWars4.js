9ee359574f7a56e844645837d17f5da4c4381815
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-06-02 21:02:44 +0200

    update

--- CodeWars4.js
+++ CodeWars4.js
@@ -7,3 +7,18 @@ function calculate(str) {
 }
 
 calculate("1plus2plus3minus4");
+
+// Powers of 3 7kyu
+function largestPower(n) {
+  let x = 0;
+
+  for (let i = 0; i < n / 3; i++) {
+    if (3 ** i < n) {
+      x = i;
+    }
+  }
+
+  return x;
+}
+
+largestPower(82);
