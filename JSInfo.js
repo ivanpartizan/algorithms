056a847ba522058a69cb2972f34ba8ffb14adcf9
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-06-24 23:24:42 +0200

    update

--- JSInfo.js
+++ JSInfo.js
@@ -25,3 +25,44 @@ if (userName == "Admin") {
 } else {
   alert("I don't know you");
 }
+
+// Repeat until the input is correct
+// Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+while (true) {
+  let number = prompt("Enter a number greater than 100");
+
+  if (number > 100 || number == null || number == "") {
+    break;
+  }
+}
+
+// Output prime numbers
+// An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+// In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+// For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+// Write the code which outputs prime numbers in the interval from 2 to n.
+
+// For n = 10 the result will be 2,3,5,7.
+
+// P.S. The code should work for any n, not be hard-tuned for any fixed value.
+let n = 10;
+
+for (let i = 2; i <= n; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) alert(i);
+}
