410101526b3fa4fdecd00f09d81f3917e2197e5b
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-07-11 23:00:48 +0200

    update

--- JSInfo.js
+++ JSInfo.js
@@ -156,3 +156,27 @@ function multiplyNumeric(obj) {
 }
 
 multiplyNumeric(menu);
+
+// Create a calculator
+// Create an object calculator with three methods:
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+let calculator = {
+  read() {
+    this.a = prompt("Enter first value");
+    this.b = prompt("Enter second value");
+  },
+
+  sum() {
+    return +this.a + +this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
