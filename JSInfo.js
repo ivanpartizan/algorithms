c260861e427d235b5af20b61b4846406f0ea206f
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-07-12 23:17:32 +0200

    update

--- JSInfo.js
+++ JSInfo.js
@@ -180,3 +180,48 @@ let calculator = {
 calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
+
+// Create new Calculator
+// Create a constructor function Calculator that creates objects with 3 methods:
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of these properties.
+// mul() returns the multiplication product of these properties.
+// For instance:
+// let calculator = new Calculator();
+// calculator.read();
+// alert("Sum=" + calculator.sum());
+// alert("Mul=" + calculator.mul());
+function Calculator() {
+  this.read = function () {
+    this.a = +prompt("Enter first number");
+    this.b = +prompt("Enter second number");
+  };
+
+  this.sum = function () {
+    return this.a + this.b;
+  };
+
+  this.mul = function () {
+    return this.a * this.b;
+  };
+}
+
+// Create new Accumulator
+// Create a constructor function Accumulator(startingValue).
+// Object that it creates should:
+// Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+// The read() method should use prompt to read a new number and add it to value.
+// In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+// Here’s the demo of the code:
+// let accumulator = new Accumulator(1); // initial value 1
+// accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
+// alert(accumulator.value); // shows the sum of these values
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    this.number = +prompt("Enter a number");
+    this.value += this.number;
+  };
+}
