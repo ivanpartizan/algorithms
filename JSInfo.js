f85e1c5a6b1098ea6f4a5054c6e618639b071968
Ivan Milenkovic <argentinaivan@hotmail.com>
2025-08-01 23:59:06 +0200

    update

--- JSInfo.js
+++ JSInfo.js
@@ -243,12 +243,21 @@ function readNumber() {
   while (true) {
     number = prompt("Please enter a number...");
 
-    if (isFinite(number)) {
-      return +number;
-    }
-
     if (number === "" || number === null) {
       return null;
     }
+
+    if (isFinite(number)) {
+      return +number;
+    }
   }
 }
+
+// A random number from min to max
+// The built-in function Math.random() creates a random value from 0 to 1 (not including 1).
+// Write the function random(min, max) to generate a random floating-point number from min to max (not including max).
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+random(1, 5);
